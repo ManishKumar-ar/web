3e5307a603fd4fe4ce70a68d63e95b6275083916
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2024-09-02 22:02:09 +0530

    Update 001. Index.js

--- 002. Week/002. Express/004. Post request using expess/001. Index.js
+++ 002. Week/002. Express/004. Post request using expess/001. Index.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const app = express();
+
+
+app.post('/', (req, res) => {
+
+  console.log(req.headers["auth"])
+  console.log(req.headers["details"])
+  
+  res.send(
+    ' i am the server and i see you password and details'
+  )
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
