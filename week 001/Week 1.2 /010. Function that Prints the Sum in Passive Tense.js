3e504d9534b0b5279ac0b7e6febde4fef7c9b6b8
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2024-09-19 22:21:51 +0530

    Create 010. Function that Prints the Sum in Passive Tense.js

---
+++ week 001/Week 1.2 /010. Function that Prints the Sum in Passive Tense.js
@@ -0,0 +1,13 @@
+
+function displayInPassiveTense(sum) {
+    console.log("The sum has been calculated as: " + sum);
+}
+
+
+let calculatedSum = 45;
+displayInPassiveTense(calculatedSum);
+
+
+//Output:
+
+The sum has been calculated as: 45
